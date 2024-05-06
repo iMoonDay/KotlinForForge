ed019cf8a419d4a5fdc9273916652fe5836b0b21
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2024-05-05 18:26:34 -0700

    Add potential workaround for broken JarJar on Forge

--- combined/build.gradle.kts
+++ combined/build.gradle.kts
@@ -1,4 +1,8 @@
+import net.neoforged.gradle.common.dependency.ResolvedJarJarArtifact
+import org.jetbrains.kotlin.com.google.common.io.Files
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import java.nio.file.FileSystems
+import kotlin.io.path.*
 
 plugins {
     kotlin("jvm")
@@ -12,10 +16,6 @@ val kffGroup = "thedarkcolour"
 
 val coroutines_version: String by project
 val serialization_version: String by project
-
-val shadow: Configuration by configurations.creating {
-    exclude("org.jetbrains", "annotations")
-}
 
 base {
     archivesName.set("kotlinforforge")
@@ -78,8 +78,17 @@ fun DependencyHandler.include(dep: Dependency, maxVersion: String? = null): Depe
 
 tasks {
     jarJar.configure {
-        from(provider { shadow.map(::zipTree).toTypedArray() })
         manifest.attributes("FMLModType" to "LIBRARY")
+
+        val patchedArtifacts = ArrayList(jarJarArtifacts.resolvedArtifacts.get())
+
+        patchedArtifacts.forEachIndexed { i, artifact ->
+            if (!artifact.file.path.contains("combined")) {
+                patchedArtifacts[i] = patchArtifact(artifact)
+            }
+        }
+
+        jarJarArtifacts.resolvedArtifacts.set(patchedArtifacts)
     }
 
     withType<KotlinCompile> {
@@ -92,4 +101,38 @@ tasks {
         dependsOn(":combined:kffmod:build")
         dependsOn(jarJar)
     }
+}
+
+// Adapted from: https://github.com/object-Object/HexDebug/commit/3875f68e7220608c98d35ec60175ee9abdbad762
+// adds a "FMLModType: LIBRARY" field to the manifest (does nothing on NeoForge, but fixes bug on Forge)
+private fun patchArtifact(oldArtifact: ResolvedJarJarArtifact): ResolvedJarJarArtifact {
+    // build/kffJarJarTmp
+    val kffJarJarTmpDirectory = project.layout.buildDirectory.asFile.get().toPath().resolve("kffJarJarTmp")
+    if (!kffJarJarTmpDirectory.exists()) kffJarJarTmpDirectory.createDirectory()
+
+    // the replacement library JAR file
+    val patchedLocation = kffJarJarTmpDirectory.resolve(oldArtifact.file.name)
+    val patchedFile = patchedLocation.toFile()
+    // the replacement artifact
+    val newArtifact = ResolvedJarJarArtifact(patchedFile, oldArtifact.version, oldArtifact.versionRange, oldArtifact.group, oldArtifact.artifact)
+
+    // don't bother patching old libs
+    if (patchedFile.exists()) {
+        return newArtifact
+    }
+
+    // make a copy of the library JAR file
+    Files.copy(oldArtifact.file, patchedFile)
+    // open the JAR as a filesystem so we can change the manifest
+    val patchedFs = FileSystems.newFileSystem(patchedLocation)
+    val manifestLoc = "/META-INF/MANIFEST.MF"
+    val manifest = patchedFs.getPath(manifestLoc).readText()
+
+    // add the FMLModType to the manifest text, ensuring it is on a new line
+    val lf = System.lineSeparator()
+    val newManifest = manifest.trimEnd() + lf + "FMLModType: LIBRARY" + lf
+    // overwrite the old manifest
+    patchedFs.getPath(manifestLoc).writeText(newManifest)
+
+    return newArtifact
 }
