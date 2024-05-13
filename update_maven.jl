2627d9963afb16668f6e045a94ff96ff45e37708
thedarkcolour <30441001+thedarkcolour@users.noreply.github.com>
2024-05-13 13:55:23 -0700

    Ensure local maven does not overwrite upstream maven metadata

--- update_maven.jl
+++ update_maven.jl
@@ -6,6 +6,24 @@ update_maven_local:
 =##
 # Publish to maven local within this project
 original_pwd = pwd()
+println("Fetching from upstream...")
+# Sync maven-metadata-local.xml with upstream
+run(`git fetch`)
+println("Syncing local Maven metadata...")
+for (root, dir, files) in walkdir("thedarkcolour")
+  for file_name in files
+    if (file_name == "maven-metadata.xml")
+      file = joinpath(root, file_name)
+      local_file = replace(file, ".xml" => "-local.xml")
+
+      if (isfile(local_file) || mtime(local_file) < mtime(file))
+        cp(file, local_file, force=true)
+      end
+    end
+  end
+end
+
+
 # Compiles KFF and publishes it to local Maven within this folder
 publish_special_maven() = run(`gradlew.bat -Dmaven.repo.local=$original_pwd publishAllMavens`)
 # Executes the Gradle task when KotlinForForge project folder is in same folder as this website folder
